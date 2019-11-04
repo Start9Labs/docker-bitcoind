fa95e2696e6fe0f479cb24c2c83a79844b709f4b
Start9 Ops <ops@start9.dev>
2019-11-04 02:53:05 +0000

    fix entrypoint

--- bin/docker_entrypoint.sh
+++ bin/docker_entrypoint.sh
@@ -11,8 +11,4 @@ HOST_IP=$(ip -4 route list match 0/0 | awk '{print $3}')
 # If config doesn't exist, initialize with sane defaults for running a
 # non-mining node.
 
-if [ $# -eq 0 ]; then
-  exec bitcoind -datadir=${BITCOIN_DIR} -conf=${BITCOIN_CONF} -proxy=$HOST_IP:9050 -torcontrol=$HOST_IP:9051 -listenonion
-else
-  exec "$@"
-fi
+exec bitcoind -datadir=${BITCOIN_DIR} -conf=${BITCOIN_CONF} -proxy=$HOST_IP:9050 -onion=$HOST_IP:9050 -torcontrol=$HOST_IP:9051 -listenonion

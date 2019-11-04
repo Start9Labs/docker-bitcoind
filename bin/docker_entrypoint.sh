be434273ed6994eaa70b4b5667d1ee545d0c664f
Aiden McClelland <me@drbonez.dev>
2019-11-03 18:01:06 -0700

    update entrypoint

--- bin/docker_entrypoint.sh
+++ bin/docker_entrypoint.sh
@@ -6,12 +6,13 @@ ip -4 route list match 0/0 | awk '{print $3 "\thost.docker.internal"}' >> /etc/h
 
 BITCOIN_DIR=/root/.bitcoin
 BITCOIN_CONF=${BITCOIN_DIR}/bitcoin.conf
+HOST_IP=$(ip -4 route list match 0/0 | awk '{print $3}')
 
 # If config doesn't exist, initialize with sane defaults for running a
 # non-mining node.
 
 if [ $# -eq 0 ]; then
-  exec bitcoind -datadir=${BITCOIN_DIR} -conf=${BITCOIN_CONF}
+  exec bitcoind -datadir=${BITCOIN_DIR} -conf=${BITCOIN_CONF} -proxy=$HOST_IP:9050 -torcontrol=$HOST_IP:9051 -listenonion
 else
   exec "$@"
 fi

2090a0f064bdfd811ca085ab263267d47d865292
Aiden McClelland <aiden.mcclelland@saltlending.com>
2020-01-07 11:41:25 -0700

    update entrypoint

--- bin/docker_entrypoint.sh
+++ bin/docker_entrypoint.sh
@@ -12,6 +12,9 @@ HOST_IP=$(ip -4 route list match 0/0 | awk '{print $3}')
 # non-mining node.
 /root/tmpl /root/config.yaml < ${BITCOIN_DIR}/bitcoin.conf.template > ${BITCOIN_CONF}
 echo "HOST IP: ${HOST_IP}"
-echo "server = ${HOST_IP}" > /etc/torsocks.conf
+echo "TorAddress ${HOST_IP}" > /etc/tor/torsocks.conf
+echo "TorPort 9050" >> /etc/tor/torsocks.conf
+echo "OnionAddrRange 127.42.42.0/24" >> /etc/tor/torsocks.conf
+echo "AllowInbound 1" >> /etc/tor/torsocks.conf
 
 exec torsocks bitcoind -datadir=${BITCOIN_DIR} -externalip=${TOR_ADDRESS} -conf=${BITCOIN_CONF}
